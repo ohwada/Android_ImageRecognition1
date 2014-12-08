5465f0df814ebf357dc606d4367730ff3eb43519
Kenichi Ohwada <ml.ohwada@gmail.com>
2014-12-08 20:55:42 +0900

    FirstVersion

--- ImageRecognition1/src/jp/ohwada/andorid/imagerecognition1/ApiConfig.java
+++ ImageRecognition1/src/jp/ohwada/andorid/imagerecognition1/ApiConfig.java
@@ -5,6 +5,6 @@ package jp.ohwada.andorid.imagerecognition1;
  */  
 public class ApiConfig {
 
-	public static final String APIKEY = "64754875774267552e735755684e444d6368714b51653065327747512e4e72443733352f786c4556634e31";
-	
+	public static final String APIKEY = "Your API Key";
+
 }
